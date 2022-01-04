897c805898ce989e18311505a641f9879ecc2207
clarech712 <clarech712@gmail.com>
2022-01-03 21:58:09 -0500

    Complete Part 1: Intro and Setting Up

---
+++ main.py
@@ -0,0 +1,14 @@
+# Pac-Man
+#
+# Name: main.py
+# Modified by: clarech712
+# Purpose: Executed when game played
+
+# Import App class
+# ??? Why "from _____ import *" and not "import _____"?
+from app_class import *
+
+# Run App
+if __name__ == "__main__":
+    app = App()
+    app.run()
